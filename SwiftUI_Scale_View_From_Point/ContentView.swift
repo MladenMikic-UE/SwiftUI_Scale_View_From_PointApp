3b31c4427bb08e396e86cc07483b2699dab9b5ba
Mladen Mikić <mladen.mikic@unseenexcellence.com>
2023-06-08 12:24:06 +0200

    Initial commit

--- SwiftUI_Scale_View_From_Point/ContentView.swift
+++ SwiftUI_Scale_View_From_Point/ContentView.swift
@@ -1,26 +1,70 @@
-//
-//  ContentView.swift
-//  SwiftUI_Scale_View_From_Point
-//
-//  Created by Mladen Mikić on 08.06.2023..
-//
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+public struct ScaleUpContainerView<Content>: View where Content: View {
+    
+    @Binding var isScaled: Bool
+        
+    func getSize(gp: GeometryProxy) -> CGSize {
+        if isScaled {
+            return CGSize(width: gp.size.width - (padding.leading + padding.trailing),
+                          height: gp.size.height - (padding.top + padding.bottom))
+        } else {
+            return CGSize(width: originSize.width, height: originSize.height)
+        }
+    }
+    
+    func getOrigintPoint(gp: GeometryProxy) -> CGPoint {
+        if isScaled {
+            return CGPoint(x: gp.size.width / 2.0,
+                           y: gp.size.height / 2.0)
+        } else {
+            return CGPoint(x: originPoint.x, y: originPoint.x)
+        }
+    }
+
+    
+    private let padding: EdgeInsets
+    private let originPoint: CGPoint
+    private let originSize: CGSize
+    private let presentationDelay: CGFloat
+    private let content: Content
+
+    public init(@ViewBuilder content: () -> Content, padding: EdgeInsets,
+                originPoint: CGPoint,
+                originSize: CGSize,
+                presentationDelay: CGFloat = 0.4,
+                isScaled: Binding<Bool>) {
+        self.content = content()
+        self.padding = padding
+        self.originSize = originSize
+        self.originPoint = originPoint
+        self.presentationDelay = presentationDelay
+        self._isScaled = isScaled
+    }
+    
+    public var body: some View {
+        
+        GeometryReader { gr in
+            
+            ZStack {
+                
+                content
+                    .edgesIgnoringSafeArea(.bottom)
+                    .frame(width: getSize(gp: gr).width, height: getSize(gp: gr).height)
+                    .position(x: getOrigintPoint(gp: gr).x, y: getOrigintPoint(gp: gr).y)
+
+            }
+
+        }
+        .onAppear {
+           
+            DispatchQueue.main.asyncAfter(deadline: .now() + presentationDelay) {
+                withAnimation {
+                    isScaled = true
+                }
+            }
+        }
+        
     }
 }
--- SwiftUI_Scale_View_From_Point/SwiftUI_Scale_View_From_PointApp.swift
+++ SwiftUI_Scale_View_From_Point/SwiftUI_Scale_View_From_PointApp.swift
@@ -1,17 +1,36 @@
-//
-//  SwiftUI_Scale_View_From_PointApp.swift
-//  SwiftUI_Scale_View_From_Point
-//
-//  Created by Mladen Mikić on 08.06.2023..
-//
-
 import SwiftUI
+
 
 @main
 struct SwiftUI_Scale_View_From_PointApp: App {
+    
+    @State var isScaled: Bool = false
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ZStack {
+                
+                Color.black.edgesIgnoringSafeArea(.all)
+                
+                ScaleUpContainerView(content: {
+                    if isScaled {
+                        Color.red
+                            .cornerRadius(20)
+                    } else {
+                        Color.white
+                            .cornerRadius(20)
+                    }
+                }, padding: .init(top: 20,
+                                  leading: 20,
+                                  bottom: 20,
+                                  trailing: 20),
+                                     originPoint: CGPoint(x: 100, y:100),
+                                     originSize: CGSize(width: 44, height: 44),
+                                     isScaled: $isScaled)
+                
+                
+            }
+
         }
     }
 }
