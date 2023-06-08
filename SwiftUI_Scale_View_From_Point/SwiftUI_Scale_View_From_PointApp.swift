c405a26e06efcbbf9e3255084bbbd726a47fa82a
Mladen Mikić <mladen.mikic@unseenexcellence.com>
2023-06-08 10:33:24 +0200

    Initial Commit

---
+++ SwiftUI_Scale_View_From_Point/SwiftUI_Scale_View_From_PointApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Scale_View_From_PointApp.swift
+//  SwiftUI_Scale_View_From_Point
+//
+//  Created by Mladen Mikić on 08.06.2023..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Scale_View_From_PointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
